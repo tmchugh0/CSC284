0ed90f2256bcf742b89bd9f698bca14bd7f488f0
ColsonDunlap <48217314+ColsonDunlap@users.noreply.github.com>
2021-02-10 20:32:26 -0500

    Create ParticipationList.java

---
+++ ParticipationList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class ParticipationList {
+
+   public static void main(String[] args) {
+      
+      ArrayList<Student> students = new ArrayList<>();
+      
+      //TODO: Create a Student Object with your first and last name and add them to the students list
+      students.add(new Student("Colson","Dunlap"));
+      
+      
+      //Sort the list by last name
+      students.sort((o1,o2) -> o1.getLastName().compareTo(o2.getLastName()));
+      
+      //print the list of students
+      for(Student student: students){
+         System.out.println(student);
+      }
+   }
+}
