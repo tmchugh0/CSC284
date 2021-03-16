a597f004c62de3a01237062d7af176067d9d5360
Malvina <80787337+code2suceed@users.noreply.github.com>
2021-03-16 17:57:35 -0400

    Create ParticipationList.java

--- ParticipationList.java
+++ ParticipationList.java
@@ -12,6 +12,7 @@ public class ParticipationList {
       students.add(new Student("Nadia", "Brandl");
       students.add(new Student("Safron","Haile"));
       students.add(new Student("Chris","Gouin"));
+      students.add(new Student("Malvina", "Muzichuk");            
 
       //Sort the list by last name
       students.sort((o1,o2) -> o1.getLastName().compareTo(o2.getLastName()));
