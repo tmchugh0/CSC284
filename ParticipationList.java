447736674893608fadc58ce71865111dab808656
DuRand W <durand.washington@yahoo.com>
2021-03-17 21:41:26 -0400

    Update ParticipationList.java
    added personal Student object to the list

--- ParticipationList.java
+++ ParticipationList.java
@@ -18,6 +18,7 @@ public class ParticipationList {
       students.add(new Student("Cameron", "Lawrence"));
       students.add(new Student("Corey", "Esposito"));
       students.add(new Student("Chase", "Perry"));
+	  students.add(new Student("DuRand", "Washington"));
 
       //Sort the list by last name
       students.sort((o1,o2) -> o1.getLastName().compareTo(o2.getLastName()));
