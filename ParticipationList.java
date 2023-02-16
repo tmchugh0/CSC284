fb3953d4c18510c1c4583ec1b61d6c2e57d87d7a
tmchugh0 <69474670+tmchugh0@users.noreply.github.com>
2023-02-16 18:03:56 -0500

    Update ParticipationList.java
    updated

--- ParticipationList.java
+++ ParticipationList.java
@@ -28,6 +28,7 @@ public class ParticipationList {
       students.add(new Student("Morgan","Moncur")); 
       students.add(new Student("Danielle","David"));
       students.add(new Student("Nick", "Kolodij"));
+      students.add(new Student("Tyler", "McHugh"));
 
       //Sort the list by last name
       students.sort((o1,o2) -> o1.getLastName().compareTo(o2.getLastName()));
